0deeaed75b324af9ca8920e51dd645d125d5989d
amandabiagi <53722053+amandabiagi@users.noreply.github.com>
2019-10-15 23:37:24 -0300

    Pagina_GodOfWar
    -História -Cadastro -Login att

---
+++ JS/funcao_cadastro.js
@@ -0,0 +1,37 @@
+function primeira_historia(){
+    texto_historia.innerHTML=`<b style="color: black">God of War (2005) – PS2</b>
+    O primeiro God of War foi a base de tudo. Ele trazia uma história bem simples: servo de Ares, o deus da guerra, Kratos batalhava na Grécia antiga contra diversas forças militares e lidava com qualquer tipo de inimigo. Mas ele tinha uma fraqueza: sua família. Para resolver a situação, Ares plantou uma armadilha e fez com que Kratos matasse sua esposa e filha, acidentalmente. Tomado pela fúria, agora o Fantasma de Esparta jurou vingança contra seu antigo mestre e decide derrotá-lo ao longo de sua primeira jornada.`;
+   
+}
+function segunda_historia(){
+    texto_historia.innerHTML=`God of War 2 (2007) – PS2
+    God of War 2 começa com Kratos no panteão grego. Após derrotar Ares, ele assume sua posição como o novo deus da guerra no Olimpo. Após uma grande batalha, Kratos recebe auxílio de Zeus, que pede, em troca, sua lealdade ao Olimpo. Ele se nega, por ainda ser atormentado com pesadelos de sua família morta. Zeus mata Kratos, que precisa recomeçar do zero, agora desafiando todos os outros deuses, jurando vingança contra qualquer um que se colocar em sua frente.`;
+}
+function terceira_historia(){
+    texto_historia.innerHTML=`<b style="color: black">God of War: Betrayal (2007) – Mobile</b>
+    God of War: Betrayal serve como “Interlúdio”, um jogo entre jogos. Ele foi lançado apenas nos celulares da Sony e mostra a campanha de Kratos em seu combate por toda a Grécia, liderando o exército espartano. O título se passa pouco antes de God of War 2 e mostra a motivação pela qual Zeus foi levado a trair Kratos, ao menos de início.`;
+}
+function quarta_historia(){
+    texto_historia.innerHTML=`God of War: Chains of Olympus (2008) – PSP, PS3
+    God of War: Chains of Olympus foi o primeiro game da saga a ser lançado em um videogame portátil. Chains of Olympus narra as missões do Deus da Guerra quando ele ainda era um servo dos deuses, antes dos fatos ocorridos no primeiro título. O jogo, contudo, acontece após o guerreiro perder sua família e aqui ele tem um reencontro inesperado com sua esposa e filha, por mais que não consiga se reunir com as duas. A grande inimiga é Perséfone, deusa das ervas.`;
+}
+function quinta_historia(){
+    texto_historia.innerHTML=`God of War 3 (2010) – PS3, PS4
+    Uma das sequências mais aguardadas, God of War 3 marca o capítulo derradeiro de Kratos contra os deuses gregos. Aqui, com todo seu poder e fúria, o Fantasma de Esparta vai para cima dos “restantes” até chegar a Zeus e, enfim, derrotá-lo. O game é um dos que possui mais sequências de ação e pouca história, ainda que trate de resolver as questões pendentes entre Kratos e sua família. Ele descobre ainda que o deus do trovão estava sendo controlado por uma força maligna, mas que não ajuda na questão do conflito. Ao final, Kratos tomba e é visto caído, sem esperança de recuperação.`;
+}
+function sexta_historia(){
+    texto_historia.innerHTML=`God of War: Ghost of Sparta (2010) – PSP, PS3
+    God of War: Ghost of Sparta foi o segundo título da série a ser lançado no PSP e, em teoria, se passa antes da grande conclusão da saga. O game mostra um pouco do passado do guerreiro, enquanto descobrimos que ele tinha um irmão, Deimos, que é um dos inimigos deste capítulo. Além disso, Kratos também entra em guerra com os titãs e a morte. Em geral, o jogo serve para fechar pontas soltas com personagens menores.`;
+}
+function setima_historia(){
+    texto_historia.innerHTML=`God of War: Ascension (2013) – PS3
+    Quando ninguém esperava mais um título God of War, a Sony anunciou God of War: Ascension, com lançamento no PS3. Ascension é um dos mais criticados da saga por não trazer muita coisa nova e apenas retornar ao passado de Kratos. Mais uma vez vemos um enredo que se passa antes do primeiro jogo, mostrando fatos de quando Kratos atuava em nome dos deuses. Aqui ele lida com as Fúrias Gregas e com uma parte de sua história onde ficou preso.
+    
+    `;
+}
+function oitava_historia(){
+    texto_historia.innerHTML=`God of War (2018) – PS4
+    O atual God of War é a verdadeira estreia da série no PS4 – apesar de o console ter recebido relançamentos e coletâneas. O título se passa após toda a saga de Kratos, mostrando o guerreiro tentando viver de maneira pacífica dentro de um ambiente nórdico, abandonando a Grécia. Ao lado de seu filho, Atreus, o Fantasma de Esparta é surpreendido com elementos de seu passado e uma nova, e possível, história envolvendo divindades. O futuro será revelado ao longo da campanha.
+    
+    `;
+}
